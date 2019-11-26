ce37a852325a9b8c95a09ea08b8ea3f0a450204a
Lawrence Whiteside <law@cinemasetfree.com>
2019-11-26 08:25:56 -0600

    quick fix

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default [
+
+  /* SLIDE 2 */
+  `
+    fg black
+    bg red
+    font 50 50% 10%
+    text Paige
+    fg #fff
+    text Paige
+  `
+  ,
+  /* SLIDE 1 */
+  `
+    fg red
+    bg black
+    font 200 50% 10%
+    text She
+    fg #222
+    text Is
+    font 200 50% +0
+    text Cute
+  `
+  ,
+
+  /* SLIDE 3 */
+  `
+    fg green
+    bg blue
+    font 100 600 50%
+    text She is cute
+
+  `
+]
--- src/index.js
+++ src/index.js
@@ -1,173 +1,5 @@
 import "./index.css"
-
-// ========================================================================
-// GRAPHICS
-// ========================================================================
-
-function debounce(fn, delay) {
-  var timer = null;
-  return function () {
-    var context = this, args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      fn.apply(context, args);
-    }, delay);
-  };
-}
-
-function throttle(fn, threshhold, scope) {
-  threshhold || (threshhold = 250);
-  var last,
-      deferTimer;
-  return function () {
-    var context = scope || this;
-
-    var now = +new Date,
-        args = arguments;
-    if (last && now < last + threshhold) {
-      // hold on to it
-      clearTimeout(deferTimer);
-      deferTimer = setTimeout(function () {
-        last = now;
-        fn.apply(context, args);
-      }, threshhold);
-    } else {
-      last = now;
-      fn.apply(context, args);
-    }
-  };
-}
-
-class Graphics {
-  constructor() {
-    this.canvas = document.querySelector('canvas');
-    this.context = this.canvas.getContext('2d');
-    window.addEventListener('resize', () => this.resize())
-    this.resize();
-    this.fps();
-  }
-
-  circle(color, x, y, r) {
-    const c = this.context;
-    c.fillStyle = color;
-    c.beginPath();
-    c.arc(x, y, r, 0, Math.PI*2);
-    c.fill();
-  }
-
-  rect(color, x, y, w, h) {
-    const c = this.context;
-    c.fillStyle = color;
-    c.fillRect(x, y, w, h);
-  }
-
-  text(text, color, size, x, y) {
-    const c = this.context;
-    c.fillStyle = color;
-    c.font = `normal bold ${size}px sans-serif`;
-    c.fillText(text, x, y)
-  }
-
-  resize() {
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-  }
-
-  fps() {
-    const now = performance.now();
-    const delta = now - this.last;
-    this.last = now
-    this.text(delta, 'red', 50, 20, 20)
-  }
-}
-
-// ========================================================================
-// LOOP
-// ========================================================================
-
-const slide1 = `
-  fg red
-  bg black
-  text Hello 200 30 600
-`
-
-const slide2 = `
-  fg black
-  bg red
-  text Goodbye 200 30 670
-`
-
-const slide3 = `
-  fg green
-  bg blue
-  text Twitter 200 300 670
-`
-
-class ReadSlide {
-  constructor(slide, graphics) {
-    this.fg = 'white'
-    this.graphics = graphics;
-    const lines = this.parse(slide)
-    lines.map(line => this.interpet(line));
-  }
-
-  interpet(line) {
-    const tokens = line.split(" ");
-    const g = this.graphics
-    switch (tokens[0]) {
-      case "bg":
-        g.rect(tokens[1], 0, 0, g.canvas.width, g.canvas.height);
-        break;
-      case "fg":
-        this.fg = tokens[1];
-        break;
-      case "text":
-        g.text(tokens[1], this.fg, parseInt(tokens[2]), parseInt(tokens[3]), parseInt(tokens[4]));
-        break;
-    }
-  }
-
-  parse(slide) {
-    return slide.split("\n").map(l => l.trim()).filter(l => l != "");
-  }
-}
-
-class Game {
-  constructor() {
-    this.graphics = new Graphics();
-    this.state = {
-      slides: [
-        slide1, slide2, slide3
-      ],
-      current: 0
-    }
-  }
-
-  start() {
-    this.loop()
-    window.addEventListener("keydown", e => this.handlePress(e), false)
-  }
-
-  handlePress(e) {
-    console.log(e.key);
-    if (e.key == "ArrowRight") this.state.current++;
-    if (e.key == "ArrowLeft") this.state.current--;
-    if (this.state.current > this.state.slides.length - 1) this.state.current = 0;
-    if (this.state.current < 0) this.state.current = this.state.slides.length - 1;
-  }
-
-  doOneFrame() {
-    const slide = this.state.slides[this.state.current]
-    console.log("CURRENT", this.state.current);
-    new ReadSlide(slide, this.graphics)
-  }
-
-  loop() {
-    this.doOneFrame()
-    requestAnimationFrame(() => this.loop())
-  }
-}
-
+import Data from "./data.js"
 
 // ========================================================================
 // INPUT
@@ -197,6 +29,198 @@ class Input {
   }
 }
 
+// ========================================================================
+// GRAPHICS
+// ========================================================================
+
+class Graphics {
+  constructor() {
+    this.canvas = document.querySelector('canvas');
+    this.context = this.canvas.getContext('2d');
+    window.addEventListener('resize', () => this.resize())
+    this.resize();
+    this.fps();
+  }
+
+  circle(color, x, y, r) {
+    const c = this.context;
+    c.fillStyle = color;
+    c.beginPath();
+    c.arc(x, y, r, 0, Math.PI*2);
+    c.fill();
+  }
+
+  rect(color, x, y, w, h) {
+    const c = this.context;
+    c.fillStyle = color;
+    c.fillRect(x, y, w, h);
+  }
+
+  text(text, color, size, x, y) {
+    const c = this.context;
+    c.fillStyle = color;
+    c.textAlign = 'center'
+    c.font = `normal bold ${size}px sans-serif`;
+    c.fillText(text, x, y)
+  }
+
+  resize() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
+
+  fps() {
+    const now = performance.now();
+    const delta = now - this.last;
+    this.last = now
+    this.text(delta, 'red', 50, 20, 20)
+  }
+}
+
+// ========================================================================
+// INTERPRETER
+// ========================================================================
+
+class ReadSlide {
+  constructor(slide, graphics) {
+    this.fg = 'white'
+    this.font = 100
+    this.x = 0
+    this.y = 0
+    this.lastY = null;
+    this.graphics = graphics;
+    this.entities = [];
+    const lines = this.parse(slide)
+    for (var i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      const entity = this.interpet(line)
+      if (entity) this.entities.push(entity)
+    }
+  }
+
+  /*
+  SAMPLE SLIDE
+  fg black
+  bg red
+  font 200 50% 50%
+  text Such a sweet feature
+  */
+  interpet(line) {
+    const t = line.split(" ");
+    const g = this.graphics
+    const cmd = t[0]
+    switch (cmd) {
+
+      case "bg":
+        g.rect(t[1], 0, 0, g.canvas.width, g.canvas.height);
+        break;
+
+      case "fg":
+        this.fg = t[1];
+        break;
+
+      case "font":
+        let x = t[2]
+        let y = t[3]
+        this.x =  (x[x.length - 1] == '%') ? (((parseInt(x.slice(0, -1))/100.0) * g.canvas.width)) : parseInt(x);
+
+        const oldFont = parseInt(this.font)
+        this.font = parseInt(t[1])
+        if (y[y.length - 1] == '%') {
+          this.y = (((parseInt(y.slice(0, -1))/100.0) * g.canvas.height))
+        } else if (y[0] == '+') {
+          const extra = parseInt(y.slice(1, y.length))
+          const lineHeight = oldFont + extra;
+          this.y += lineHeight;
+        } else {
+          this.y = parseInt(y);
+        }
+        break;
+
+      case "text":
+        console.log(this.lastY, this.y);
+        if (this.lastY == this.y) {
+          this.y += parseInt(this.font);
+        }
+        t.shift()
+        g.text(t.join(" "), this.fg, this.font, this.x, this.y);
+        return { x: this.x, y: this.y};
+        break;
+    }
+    this.lastY = this.y;
+  }
+
+  parse(slide) {
+    return slide.split("\n").map(l => l.trim()).filter(l => l != "");
+  }
+
+  export() {
+    return this.entities
+  }
+}
+
+// ========================================================================
+// GAME LOOP
+// ========================================================================
+
+class Game {
+  constructor() {
+    this.graphics = new Graphics();
+    this.debugger = new Debug(this.graphics)
+    this.state = {
+      slides: Data,
+      current: 0,
+      debugging: false
+    }
+  }
+
+  start() {
+    this.loop()
+    window.addEventListener("keydown", e => this.handlePress(e), false)
+  }
+
+  handlePress(e) {
+    console.log(e.key);
+    if (e.key == "ArrowRight") this.state.current++;
+    if (e.key == "ArrowLeft") this.state.current--;
+    if (e.key == "Escape") this.state.debugging = !this.state.debugging;
+    if (this.state.current > this.state.slides.length - 1) this.state.current = 0;
+    if (this.state.current < 0) this.state.current = this.state.slides.length - 1;
+  }
+
+  doOneFrame() {
+    const slide = this.state.slides[this.state.current]
+    const reader = new ReadSlide(slide, this.graphics)
+    if (this.state.debugging) {
+      const ex = reader.export();
+      // console.log(ex)
+      ex.map(e => this.debugger.draw(e));
+    }
+  }
+
+  loop() {
+    this.doOneFrame()
+    requestAnimationFrame(() => this.loop())
+  }
+}
+
+
+// ========================================================================
+// DEBUG
+// ========================================================================
+
+class Debug {
+  constructor(graphics) {
+    this.graphics = graphics;
+  }
+
+  draw(e) {
+    const { x, y } = this;
+    const g = this.graphics
+    g.text(`X: ${x}`, 'white', 30, x, y)
+    g.text(`Y: ${y}`, 'white', 30, x, y+30)
+  }
+}
 
 // ========================================================================
 // STARTUP
